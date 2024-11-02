730137ac51c8ebb574654743b36a7c66d39922cd
NewGen2022 <amirgimari@gmail.com>
2024-11-02 13:36:50 +0200

    added: middleware to handle different range of request types; new auth route

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const authRouter = require('./routes/auth.js');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>Bye planet?</h1>');
-});
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// ROUTES
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
