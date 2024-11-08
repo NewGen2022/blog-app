f758552ca54bb8ba598a80a0154cb592cd39fc46
NewGen2022 <amirgimari@gmail.com>
2024-11-08 11:42:23 +0200

    added controllers: login, token: for refreshing access token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/authController');
-const { validateRegistration } = require('../middleware/validation');
+const {
+    registerController,
+    loginController,
+    tokenController,
+} = require('../controllers/authController');
+const { validateRegistration } = require('../middleware/auth');
 
-router.post('/register', validateRegistration, register);
+router.post('/register', validateRegistration, registerController);
+router.post('/login', loginController);
+router.post('/token', tokenController);
 
 module.exports = router;
