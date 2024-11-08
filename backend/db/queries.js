9bd643414f77cd19af0817771a2207833f4bf281
NewGen2022 <amirgimari@gmail.com>
2024-11-08 12:05:30 +0200

    added queries for: get/create refresh token, get user by username

--- backend/db/queries.js
+++ backend/db/queries.js
@@ -2,10 +2,10 @@ const prisma = require('./prismaClient');
 const bcrypt = require('bcryptjs');
 
 // CREATE QUERIES
-const registerUser = async (username, password, role = 'AUTHOR') => {
+const registerDB = async (username, password, role = 'AUTHOR') => {
     try {
         // Password hashing
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, 12);
 
         const user = await prisma.user.create({
             data: {
@@ -20,11 +20,57 @@ const registerUser = async (username, password, role = 'AUTHOR') => {
         console.error('Error creating user:', err);
 
         if (err.code === 'P2002') {
-            throw new Error('Username already exists');
+            throw new Error('Error adding user to DB. Username already exists');
         }
 
-        throw new Error('Failed to register user. Please try again.');
+        throw new Error('Failed to register user in DB. Please try again!');
     }
 };
 
-module.exports = { registerUser };
+const createRefreshTokenDB = async (refreshToken, userId) => {
+    try {
+        const refreshTokenExpiration = new Date();
+        refreshTokenExpiration.setDate(refreshTokenExpiration.getDate() + 7); // Set expiration for 7 days
+
+        await prisma.refreshToken.create({
+            data: {
+                token: refreshToken,
+                expiresAt: refreshTokenExpiration,
+                userId: userId,
+            },
+        });
+    } catch (err) {
+        console.error('Error saving refresh token:', err);
+        throw new Error('Error saving refresh token to database');
+    }
+};
+
+// GET QUERIES
+const getUserByUsernameDB = async (username) => {
+    try {
+        return await prisma.user.findUnique({
+            where: { username: username },
+        });
+    } catch (err) {
+        console.error('Error fetching user by username:', err);
+        throw new Error('Error fetching user data');
+    }
+};
+
+const getRefreshTokenDB = async () => {
+    try {
+        return await prisma.refreshToken.findUnique({
+            where: { token: refreshToken },
+        });
+    } catch (err) {
+        console.error('Error fetching refresh token:', err);
+        throw new Error('Error fetching refresh token');
+    }
+};
+
+module.exports = {
+    registerDB,
+    getUserByUsernameDB,
+    getRefreshTokenDB,
+    createRefreshTokenDB,
+};
