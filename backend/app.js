36a932b260520941a826b971001d7e6bcaffb779
NewGen2022 <amirgimari@gmail.com>
2024-11-13 17:08:33 +0200

    added cors options for frontend to reader

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const authRouter = require('./routes/auth.js');
 
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+    origin: 'http://localhost:8877',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
