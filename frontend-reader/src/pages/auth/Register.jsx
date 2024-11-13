3155fb3278c45ff3776ad2c51f4fca5092bf0ec0
NewGen2022 <amirgimari@gmail.com>
2024-11-13 17:10:10 +0200

    added registration form skeleton and post requestto register a user

--- frontend-reader/src/pages/auth/Register.jsx
+++ frontend-reader/src/pages/auth/Register.jsx
@@ -1,5 +1,104 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 const Register = () => {
-    return <div>REGISTER</div>;
+    const [registerData, setRegisterData] = useState({
+        username: '',
+        password: '',
+        confirmPassword: '',
+    });
+
+    const [errors, setErrors] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setRegisterData({ ...registerData, [name]: value });
+    };
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault();
+
+        setErrors('');
+
+        if (registerData.password !== registerData.confirmPassword) {
+            setErrors('Passwords do not match');
+            return;
+        }
+
+        try {
+            await axios.post('http://localhost:2020/api/auth/register', {
+                username: registerData.username,
+                password: registerData.password,
+                confirm_password: registerData.confirmPassword,
+            });
+
+            navigate('/auth/login');
+        } catch (err) {
+            if (err.response) {
+                if (err.response.data.errors) {
+                    const backendErrors = err.response.data.errors.map(
+                        (err) => err.msg
+                    );
+                    setErrors(backendErrors.join(', '));
+                } else {
+                    setErrors('An error occurred during registration.');
+                }
+            } else {
+                setErrors('Network error or server not reachable.');
+            }
+        }
+    };
+
+    return (
+        <form
+            id="registerForm"
+            className="form"
+            onSubmit={handleRegisterSubmit}
+        >
+            <div id="formHeader">Register</div>
+
+            <div id="registerErrors" className="authError">
+                {errors && <p>{errors}</p>}
+            </div>
+
+            <label htmlFor="username">Username *</label>
+            <input
+                type="text"
+                name="username"
+                id="username"
+                value={registerData.username}
+                onChange={handleChange}
+                required
+            />
+
+            <label htmlFor="password">Password *</label>
+            <input
+                type="password"
+                name="password"
+                id="password"
+                onChange={handleChange}
+                required
+            />
+
+            <label htmlFor="confirmPassword">Confirm password *</label>
+            <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                onChange={handleChange}
+                required
+            />
+
+            <button type="submit">Register</button>
+
+            <div className="haveAnAccount">
+                Already have an account? <a href="/auth/login">Log In</a>
+            </div>
+        </form>
+    );
 };
 
 export default Register;
