8ef8f279c04288ef664b21f4348c8cae43b51f35
NewGen2022 <amirgimari@gmail.com>
2024-11-15 18:09:59 +0200

    added main routes component

--- frontend-reader/src/App.jsx
+++ frontend-reader/src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import AuthRoutes from './pages/auth/AuthRoutes';
+import MainRoutes from './pages/main/mainRoutes';
 
 function App() {
     return (
         <Routes>
             <Route path="/auth/*" element={<AuthRoutes />} />
+            <Route path="/*" element={<MainRoutes />} />
         </Routes>
     );
 }
