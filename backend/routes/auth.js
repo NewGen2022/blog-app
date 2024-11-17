78b653c129206bdcc150ce26a473bea0b690f5a6
NewGen2022 <amirgimari@gmail.com>
2024-11-17 16:33:56 +0200

    added logout route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,12 +3,14 @@ const router = express.Router();
 const {
     registerController,
     loginController,
+    logoutController,
     tokenController,
 } = require('../controllers/authController');
 const { validateRegistration } = require('../middleware/auth');
 
 router.post('/register', validateRegistration, registerController);
 router.post('/login', loginController);
+router.post('/logout', logoutController);
 router.post('/token', tokenController);
 
 module.exports = router;
