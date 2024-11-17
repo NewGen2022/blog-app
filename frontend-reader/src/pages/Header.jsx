c01994bb404df87a997ed5feb5d956a660cb7a97
NewGen2022 <amirgimari@gmail.com>
2024-11-17 16:31:10 +0200

    added: setUser, handling logout, icons, classes and ids

--- frontend-reader/src/pages/Header.jsx
+++ frontend-reader/src/pages/Header.jsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 import '../styles/header.css';
+import axios from 'axios';
 
 const Header = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+
+        if (accessToken) {
+            try {
+                const decodedToken = jwtDecode(accessToken);
+                setUser({
+                    username: decodedToken.name,
+                    role: decodedToken.role,
+                });
+            } catch (err) {
+                console.error('Error decoding token:', err);
+                setUser(null);
+            }
+        } else {
+            setUser(null);
+        }
+    }, []);
+
+    const handleLogout = async (e) => {
+        e.preventDefault();
+
+        const refreshToken = localStorage.getItem('refreshToken');
+
+        if (refreshToken) {
+            try {
+                await axios.post('http://localhost:2020/api/auth/logout', {
+                    refreshToken: refreshToken,
+                });
+
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('refreshToken');
+
+                setUser(null);
+            } catch (err) {
+                console.error('Error logging out:', err);
+            }
+        } else {
+            console.log('No refresh token found');
+        }
+    };
+
     return (
         <div id="header">
             <a id="headerLeftPart" href="/">
@@ -9,16 +56,36 @@ const Header = () => {
             </a>
 
             <div id="headerRightPart">
-                <div id="headerLoginStatus">Not logged in</div>
+                <div id="headerLoginStatus">
+                    {user ? `Logged in as ${user.username}` : 'Not logged in'}
+                </div>
                 <div id="headerAuthActions">
-                    <a href="/auth/login" id="headerLogin">
-                        <img src="../../public/login.png" />
-                        Log In
-                    </a>
-                    <a href="/auth/register" id="headerRegister">
-                        <img src="../../public/register.png" />
-                        Register
-                    </a>
+                    {user ? (
+                        <a href="/" id="headerLogout" onClick={handleLogout}>
+                            <img
+                                src="../../public/logout.png"
+                                alt="Logout Icon"
+                            />
+                            Logout
+                        </a>
+                    ) : (
+                        <>
+                            <a href="/auth/login" id="headerLogin">
+                                <img
+                                    src="../../public/login.png"
+                                    alt="Login Icon"
+                                />
+                                Log In
+                            </a>
+                            <a href="/auth/register" id="headerRegister">
+                                <img
+                                    src="../../public/register.png"
+                                    alt="Register Icon"
+                                />
+                                Register
+                            </a>
+                        </>
+                    )}
                 </div>
             </div>
         </div>
