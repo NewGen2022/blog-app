003f5cd49d1a5349272f18d0cc6e687f4c9f1624
NewGen2022 <amirgimari@gmail.com>
2024-11-23 12:41:33 +0200

    deleted checkAndRefreshToken function because not using it

--- frontend-reader/src/js/TokensHandling.js
+++ frontend-reader/src/js/TokensHandling.js
@@ -7,7 +7,7 @@ const refreshAccessToken = async (refreshToken) => {
     try {
         const {
             data: { accessToken },
-        } = await axios.post('http://localhost:2020/api/auth/refresh', {
+        } = await axios.post('http://localhost:2020/api/auth/token', {
             token: refreshToken,
         });
 
@@ -25,21 +25,4 @@ const isTokenExpired = (token) => {
     return exp < Math.floor(Date.now() / 1000);
 };
 
-const checkAndRefreshToken = async () => {
-    const accessToken = localStorage.getItem('accessToken');
-    const refreshToken = localStorage.getItem('refreshToken');
-
-    if (!accessToken || !refreshToken || isTokenExpired(accessToken)) {
-        if (!refreshToken) {
-            throw new Error('Please log in again!');
-        }
-        try {
-            return await refreshAccessToken(refreshToken); // Try refreshing the access token
-        } catch (err) {
-            throw new Error('Please log in again!');
-        }
-    }
-    return accessToken; // Return the valid access token
-};
-
-export { checkAndRefreshToken, isTokenExpired, refreshAccessToken };
+export { isTokenExpired, refreshAccessToken };
