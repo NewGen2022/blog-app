5bc0bdb01de58d04bc5798d5e35ffdaa68a84217
NewGen2022 <amirgimari@gmail.com>
2024-11-23 12:47:45 +0200

    template controllers for admin routes

---
+++ backend/controllers/postsAdmin.js
@@ -0,0 +1,17 @@
+const getAllPosts = () => {};
+const getAllDraftPosts = () => {};
+const getPost = () => {};
+const createPost = () => {};
+const updatePost = () => {};
+const updatePostStatus = () => {};
+const deletePost = () => {};
+
+module.exports = {
+    getAllPosts,
+    getAllDraftPosts,
+    getPost,
+    createPost,
+    updatePost,
+    updatePostStatus,
+    deletePost,
+};
