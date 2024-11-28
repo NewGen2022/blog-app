b3e1efe35571433b59c769630764c1606c79b99a
NewGen2022 <amirgimari@gmail.com>
2024-11-28 13:14:09 +0200

    added routes for adding, deleting and getting comments

--- backend/routes/postsAdmin.js
+++ backend/routes/postsAdmin.js
@@ -8,17 +8,23 @@ const {
     updatePost,
     updatePostStatus,
     deletePost,
+    getComments,
+    addComment,
+    deleteComment,
 } = require('../controllers/postsAdmin');
 
 router.get('/all', getAllPosts); // get all published posts
 router.get('/draft', getAllDraftPosts);
 router.get('/:postId', getPost); // Get a single post by ID (both draft and published)
+router.get('/:postId/comments', getComments);
 
 router.post('/', createPost);
+router.post('/:postId/comment', addComment);
 
 router.put('/:postId', updatePost); // Update an existing post (both draft and published)
-router.put('/:postId/status', updatePostStatus);
+router.put('/:postId/status', updatePostStatus); // Only admin can update post status
 
 router.delete('/:postId', deletePost);
+router.delete('/:postId/comment/:commentId', deleteComment); // Only admin can delete comments
 
 module.exports = router;
