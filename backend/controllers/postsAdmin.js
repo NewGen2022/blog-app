73cd3018e906130699649c33d8d2d455ffddc7e5
NewGen2022 <amirgimari@gmail.com>
2024-12-02 12:20:51 +0200

    added controller to get draft posts

--- backend/controllers/postsAdmin.js
+++ backend/controllers/postsAdmin.js
@@ -18,7 +18,17 @@ const getAllPosts = async (req, res) => {
     }
 };
 
-const getAllDraftPosts = () => {};
+const getAllDraftPosts = async (req, res) => {
+    try {
+        const posts = await getAllPostsDB('DRAFT');
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json({
+            message: 'Error fetching posts',
+            error: err.message,
+        });
+    }
+};
 const getPost = () => {};
 
 const createPost = async (req, res) => {
