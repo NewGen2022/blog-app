a84c68594446773c41e4868a1b6686656fc1d2d1
NewGen2022 <amirgimari@gmail.com>
2024-12-17 08:43:51 +0200

    component that checks is user logged in

---
+++ frontend-reader/src/hooks/useAuth.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
+
+const useAuth = () => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const accessToken = localStorage.getItem('accessToken');
+
+        if (accessToken) {
+            try {
+                const decodedToken = jwtDecode(accessToken);
+                setUser({
+                    username: decodedToken.name,
+                    role: decodedToken.role,
+                });
+            } catch (err) {
+                console.error('Error decoding token:', err);
+                setUser(null);
+            }
+        } else {
+            setUser(null);
+        }
+        setLoading(false);
+    }, []);
+
+    return { user, loading };
+};
+
+export default useAuth;
