6703545941770ec6c87f6d3885011d5b8f0ac86b
NewGen2022 <amirgimari@gmail.com>
2024-12-17 13:20:07 +0200

    added sidebar logic

--- frontend-reader/src/pages/RightNavBar.jsx
+++ frontend-reader/src/pages/RightNavBar.jsx
@@ -1,10 +1,26 @@
-import '../styles/rightNavBar.css';
 import PropTypes from 'prop-types';
 
 const RightNavBar = ({ user }) => {
     return (
         <>
-            <div id="right-nav-bar">RIGHT NAVBAR</div>
+            <div id="right-nav-bar">
+                <div id="search-container">
+                    <h3>Search</h3>
+                    <input id="search-container" type="search" />
+                </div>
+                {user && user.role === 'ADMIN' ? (
+                    <>
+                        <a href="/admin/create" id="create-post-link">
+                            Create post
+                        </a>
+                        <button id="add-tag-btn">Add tag</button>
+                    </>
+                ) : (
+                    ''
+                )}
+
+                <div>All tags</div>
+            </div>
         </>
     );
 };
