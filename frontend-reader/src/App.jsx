4029fdccc1db9f4af7ec5c1f925a2d908d9f6fbb
NewGen2022 <amirgimari@gmail.com>
2024-12-17 08:35:01 +0200

    added admin routes

--- frontend-reader/src/App.jsx
+++ frontend-reader/src/App.jsx
@@ -1,13 +1,25 @@
 import './styles/App.css';
 import { Route, Routes } from 'react-router-dom';
 import AuthRoutes from './pages/auth/AuthRoutes';
-import MainRoutes from './pages/main/mainRoutes';
+import ReaderRoutes from './pages/reader/ReaderRoutes';
+import AdminRoutes from './pages/admin/AdminRoutes';
+import AdminRoute from './hooks/AdminRoute';
 
 function App() {
     return (
         <Routes>
             <Route path="/auth/*" element={<AuthRoutes />} />
-            <Route path="/*" element={<MainRoutes />} />
+
+            <Route
+                path="/admin/*"
+                element={
+                    <AdminRoute>
+                        <AdminRoutes />
+                    </AdminRoute>
+                }
+            />
+
+            <Route path="/*" element={<ReaderRoutes />} />
         </Routes>
     );
 }
