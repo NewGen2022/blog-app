c9db55c7be3b3438b616964a75805e427c5977e4
NewGen2022 <amirgimari@gmail.com>
2024-12-19 22:41:26 +0200

    added routes for tags, changed route order

--- backend/routes/postsAdmin.js
+++ backend/routes/postsAdmin.js
@@ -5,26 +5,30 @@ const {
     getAllDraftPosts,
     getPost,
     createPost,
+    createTag,
     updatePost,
     updatePostStatus,
     deletePost,
     getComments,
+    getTags,
     addComment,
     deleteComment,
 } = require('../controllers/postsAdmin');
 
 router.get('/all', getAllPosts); // get all published posts
 router.get('/draft', getAllDraftPosts);
+router.get('/tags', getTags);
 router.get('/:postId', getPost); // Get a single post by ID (both draft and published)
 router.get('/:postId/comments', getComments);
 
 router.post('/', createPost);
+router.post('/tag', createTag);
 router.post('/:postId/comment', addComment);
 
-router.put('/:postId', updatePost); // Update an existing post (both draft and published)
 router.put('/:postId/status', updatePostStatus); // Only admin can update post status
+router.put('/:postId', updatePost); // Update an existing post (both draft and published)
 
-router.delete('/:postId', deletePost);
 router.delete('/:postId/comment/:commentId', deleteComment); // Only admin can delete comments
+router.delete('/:postId', deletePost);
 
 module.exports = router;
