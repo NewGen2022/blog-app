f0a077541e18b7a68ce4f5ec20874d87bdc0bde7
NewGen2022 <amirgimari@gmail.com>
2024-12-21 11:22:32 +0200

    added displaying for all posts

--- frontend-reader/src/pages/Posts.jsx
+++ frontend-reader/src/pages/Posts.jsx
@@ -1,9 +1,12 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Layout from './layouts/Layout';
+import axios from 'axios';
+import { formatDate } from '../js/Date';
+import '../styles/posts/posts.css';
+import AlertMessage from './layouts/AlertMessage';
 
 const Posts = () => {
-    const [cards, setCards] = useState(null);
+    const [draftCards, setDraftCards] = useState(null);
     const [loading, setLoading] = useState(true);
     const [errors, setErrors] = useState(null);
 
@@ -11,18 +14,57 @@ const Posts = () => {
         axios
             .get('http://localhost:2020/api/posts/all')
             .then((response) => {
-                setCards(response.data);
+                setDraftCards(response.data);
                 setLoading(false);
             })
             .catch((err) => {
-                setErrors(err);
+                setErrors(
+                    'An error occurred while fetching posts. Please try again later.'
+                );
                 setLoading(false);
             });
     }, []);
 
     return (
         <Layout>
-            <div id="main-blog">{console.log(cards)}</div>
+            <div id="main-blog">
+                {loading ? (
+                    <div className="loading"></div>
+                ) : errors ? (
+                    <AlertMessage message={errors} type="error" />
+                ) : draftCards && draftCards.length > 0 ? (
+                    draftCards.map((card) => (
+                        <a
+                            key={card.id}
+                            href={`/post/${card.id}`}
+                            className="post"
+                        >
+                            <div className="post-updatedAt">
+                                {formatDate(card.updatedAt)}
+                            </div>
+
+                            <h2 className="post-title">{card.name}</h2>
+
+                            <div
+                                className="post-content"
+                                dangerouslySetInnerHTML={{
+                                    __html: card.content,
+                                }}
+                            ></div>
+
+                            <div className="bottom-post-container">
+                                <div className="post-comment">
+                                    {card._count.comment} 🗨️
+                                </div>
+
+                                <a className="read-more">Read more →</a>
+                            </div>
+                        </a>
+                    ))
+                ) : (
+                    <div className="no-posts">No posts yet</div>
+                )}
+            </div>
         </Layout>
     );
 };
